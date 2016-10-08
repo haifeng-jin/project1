3b6e969d256d55e1acd9ad31b5bdce3740314ddd
Haifeng Jin <jhfjhfj1@gmail.com>
2016-10-08 16:57:59 -0500

    frontend

---
+++ node/public/js/pagination.js
@@ -0,0 +1,60 @@
+    var current_page = 1;
+    var per_page = 50;
+    var table = $("#contact_list");
+    var pagination = $("#pagination");
+    var left_page = pagination.children().first();
+    var right_page = pagination.children().last();
+    var tr;
+    var row_num;
+    var page_num;
+    var rows = [];
+
+    function init_paginator() {
+        tr = table.children("a");
+        row_num = tr.length;
+        page_num = Math.ceil(row_num / per_page);
+        if (page_num < 2) {
+            pagination.html("");
+            return;
+        }
+        for (var i = 0; i < row_num; i++) {
+            rows.push(tr[i]);
+        }
+        table.html("");
+        createPaginator();
+        update(current_page);
+    }
+
+    function createPaginator() {
+        left_page.attr("onclick", "update(current_page - 1)");
+        right_page.attr("onclick", "update(current_page + 1)");
+        for (var i = 0; i < page_num; i++) {
+            var li = $("<li>");
+            var a = $("<a>");
+            a.html(i + 1);
+            a.attr("onclick", "update(" + (i + 1) + ")");
+            li.append(a);
+            li.insertBefore(right_page);
+        }
+    }
+
+    function update(a) {
+        if (a < 1 || a > page_num)
+            return;
+        current_page = a;
+        var li = pagination.children("li");
+        for (var i = 0; i < li.length; i++) {
+            li[i].removeAttribute("class");
+        }
+        if (current_page === 1)
+            left_page.attr("class", "disabled");
+        if (current_page === page_num)
+            right_page.attr("class", "disabled");
+        pagination.children("li")[current_page].setAttribute("class", "active");
+
+        table.html("");
+        var start = (current_page - 1) * per_page;
+        for (var i = start; i < start + per_page; i++) {
+            table.append(rows[i]);
+        }
+    }
--- node/public/js/util.js
+++ node/public/js/util.js
@@ -1,29 +1,83 @@
 var contact_list;
+var current_contact_id;
 
 function create_new_contact() {
-    console.log("create_new_contact");
     update_list();
     $('#create_modal').modal('hide');
 }
 
 function update_list() {
+    var list_html = $("#contact_list").html("");
     for (var i = 0; i < contact_list.length; i++) {
         var d = contact_list[i];
         var $a = $("<a>", {"class": "list-group-item list-group-item-action", "onclick": "show_detail(" + d.contact_id + ")"});
         var $h5 = $("<h5>", {"class": "list-group-item-heading"});
-        $h5.html(d.firstname + " " + d.lastname);
+        $h5.html(d.name);
         var $p = $("<p>", {"class": "list-group-item-text"});
-        $p.html("Phone: " + d.phone);
+        $p.html("Phone: " + d.phone_number);
         $a.append($h5);
         $a.append($p);
-        $("#contact_list").append($a);
+        list_html.append($a);
     }
+    init_paginator();
 }
 
-function init() {
-    $.ajax({ url: '/mock/list.txt', success: function(data) {
-        contact_list = $.parseJSON(data);
-        console.log(contact_list);
+function init_list() {
+    $.ajax({ url: '/rest?state=Jiangxi&gender=Female', success: function(data) {
+        contact_list = data;
         update_list();
+    } });
+}
+
+function get_contact(id) {
+    return contact_list.find(function (d) { return d.contact_id==id; });
+}
+
+function show_detail(id) {
+    current_contact_id = id;
+    fill_in_detail_modal(get_contact(id));
+    $('#detail_modal').modal('show');
+}
+
+function update_contact(id) {
+    $('#detail_modal').modal('hide');
+    fill_in_update_modal(get_contact(id));
+    $('#update_modal').modal('show');
+}
+
+function submit_update_contact(id) {
+    $('#update_modal').modal('hide');
+}
+
+function fill_in_detail_modal(contact) {
+    $.ajax({ url: ("/rest/relation/"+contact.contact_id), success: function(data) {
+        $('#firstname_detail').html(contact.name.split(" ")[0]);
+        $('#lastname_detail').html(contact.name.split(" ")[1]);
+        $('#phone_detail').html(contact.phone_number);
+        $('#email_detail').html(contact.email);
+        $('#gender_detail').html(contact.gender_name);
+        $('#country_detail').html(contact.country);
+        $('#state_detail').html(contact.state);
+        $('#city_detail').html(contact.city);
+        var relation = data[0];
+        for (var i = 1; i < data.length; i++) {
+            relation += " ";
+            relation += data[i];
+        }
+        $('#relation_detail').html(relation);
+    } });
+}
+
+function fill_in_update_modal(contact) {
+    $.ajax({ url: ("/rest/relation/"+contact.contact_id), success: function(data) {
+        $('#firstname_update').val(contact.name.split(" ")[0]);
+        $('#lastname_update').val(contact.name.split(" ")[1]);
+        $('#phone_update').val(contact.phone_number);
+        $('#email_update').val(contact.email);
+        $('#gender_update').val(contact.gender_name);
+        $('#country_update').val(contact.country);
+        $('#state_update').val(contact.state);
+        $('#city_update').val(contact.city);
+        $('#relation_update').val(data[0]);
     } });
 }
